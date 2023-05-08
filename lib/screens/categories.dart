cfe4caa768d89f60cccd6bc52ddc897af50c9806
Emanuel Vargas <reponzo01@gmail.com>
2023-05-08 19:12:33 -0400

    164. Passing functions through multiple layers of widgets (for state management)

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/category.dart';
+import 'package:meals/models/meal.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({Key? key}) : super(key: key);
+  const CategoriesScreen({
+    Key? key,
+    required this.onToggleFavorite,
+  }) : super(key: key);
+
+  final void Function(Meal meal) onToggleFavorite;
 
   void _selectCategory(BuildContext context, Category category) {
     final filteredMeals = dummyMeals
@@ -17,6 +23,7 @@ class CategoriesScreen extends StatelessWidget {
         builder: (ctx) => MealsScreen(
           title: category.title,
           meals: filteredMeals,
+          onToggleFavorite: onToggleFavorite,
         ),
       ),
     );
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -8,15 +8,20 @@ class MealsScreen extends StatelessWidget {
     Key? key,
     this.title,
     required this.meals,
+    required this.onToggleFavorite,
   }) : super(key: key);
 
   final String? title;
   final List<Meal> meals;
+  final void Function(Meal meal) onToggleFavorite;
 
   void selectMeal(BuildContext context, Meal meal) {
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (ctx) => MealDetailsScreen(meal: meal),
+        builder: (ctx) => MealDetailsScreen(
+          meal: meal,
+          onToggleFavorite: onToggleFavorite,
+        ),
       ),
     );
   }
