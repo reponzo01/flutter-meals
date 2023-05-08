e42ea3392d078d646c7673f40ca2f27e9253a7d9
Emanuel Vargas <reponzo01@gmail.com>
2023-05-08 14:53:12 -0400

    152. Widgets vs screens

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({
+    Key? key,
+    required this.category,
+  }) : super(key: key);
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
